abed47fd37a65c870883949b606b60a0a0bacbd9
root <root@kali>
2014-06-11 21:20:21 +0300

    khalid

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaapplication1;
+import javax.swing.*;
+/**
+ *
+ * @author root
+ */
+public class JavaApplication1 {
+
+    
+public static void main(String [] args){
+double x = 10;
+System.out.print(Math.sin(1));
+
+
+
+
+
+
+}}
